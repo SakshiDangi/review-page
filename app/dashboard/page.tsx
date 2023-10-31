f9608c58e8494497f1400871d40b7d73e0775119
sakshi <panchalsakshi01@gmail.com>
2023-10-31 09:03:36 -0700

    initializing project using next.js, typescript, tailwindcss, shadcn

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>page is right here</div>
+  )
+}
+
+export default page;
---
+++ app/details/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div>page afn  jafop</div>
+  )
+}
+
+export default Details;
